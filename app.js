ad770f054aab1923a7b1c65189bba88d59bac6dc
ckc6842 <ckc6842@gmail.com>
2015-04-03 11:57:16 +0900

    change wait interval in app.js

--- app.js
+++ app.js
@@ -20,14 +20,14 @@ function doEach(query, inde) {
         console.log("===== in - start =====");
     });
 
-    casper.wait(2000, function() {
+    casper.wait(3000, function() {
         console.log("===== in - then - 1 =====");
         this.capture("capture/" + inde.toString() + "_1.png");
         this.fill('form[action="/search/searchresult.jsp"]', { queryText: query, newsearch: 'true' }, false);
         this.click('button.js-search.Search-submit.Button.btn-search');
     });
 
-    casper.wait(5000, function() {
+    casper.wait(10000, function() {
         // aggregate results for the 'casperjs' search
         console.log("===== in - then - 2 =====");
         this.capture("capture/" + inde.toString() + "_2.png");
@@ -35,13 +35,13 @@ function doEach(query, inde) {
         this.click('div.detail h3 a');
     });
 
-    casper.wait(2000, function() {
+    casper.wait(3000, function() {
         console.log("===== in - then - 3 =====");
         this.capture("capture/" + inde.toString() + "_3.png");
         this.click('a#full-text-html');
     });
 
-    casper.wait(2000, function() {
+    casper.wait(3000, function() {
         console.log("===== in - then - 4 =====");
         this.capture("capture/" + inde.toString() + "_4.png");
         var fs = require('fs');

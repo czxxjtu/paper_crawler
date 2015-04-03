57862c1c41d6ebb614a0c561b32edbdd8e9371b1
ckc6842 <ckc6842@gmail.com>
2015-04-03 11:55:16 +0900

    update logging in app.js

--- app.js
+++ app.js
@@ -9,18 +9,19 @@ var casper = require('casper').create({
 
 casper.start('http://ieeexplore.ieee.org/Xplore/home.jsp', function() {
     // search for 'casperjs' from google form
-    console.log("===== in - start");
+    console.log("===== in - start =====");
 });
 
 function doEach(query, inde) {
     
     casper.thenOpen('http://ieeexplore.ieee.org/Xplore/home.jsp', function() {
         // search for 'casperjs' from google form
-        console.log("===== in - start");
+        console.log("[ " + inde.toString() + ": " + query + " ]")
+        console.log("===== in - start =====");
     });
 
     casper.wait(2000, function() {
-        console.log("===== in - then - 1");
+        console.log("===== in - then - 1 =====");
         this.capture("capture/" + inde.toString() + "_1.png");
         this.fill('form[action="/search/searchresult.jsp"]', { queryText: query, newsearch: 'true' }, false);
         this.click('button.js-search.Search-submit.Button.btn-search');
@@ -28,20 +29,20 @@ function doEach(query, inde) {
 
     casper.wait(5000, function() {
         // aggregate results for the 'casperjs' search
-        console.log("===== in - then - 2");
+        console.log("===== in - then - 2 =====");
         this.capture("capture/" + inde.toString() + "_2.png");
         // this.capture("capture.png");
         this.click('div.detail h3 a');
     });
 
     casper.wait(2000, function() {
-        console.log("===== in - then - 3");
+        console.log("===== in - then - 3 =====");
         this.capture("capture/" + inde.toString() + "_3.png");
         this.click('a#full-text-html');
     });
 
     casper.wait(2000, function() {
-        console.log("===== in - then - 4");
+        console.log("===== in - then - 4 =====");
         this.capture("capture/" + inde.toString() + "_4.png");
         var fs = require('fs');
         fs.write("html/" + inde.toString() + ".html", this.getHTML(), 'w');
@@ -52,11 +53,11 @@ function doEach(query, inde) {
 }
 
 for(i in array) {
-    console.log(array[i]);
+    console.log(i.toString() + ": " + array[i]);
     doEach(array[i], i);
 }
 
 casper.run(function() {
-    console.log("===== in - run");
+    console.log("===== in - run =====");
     this.exit();
 });
